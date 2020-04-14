611f1908d9040ced007f54dd3085e41437b7b1ef
Nathália Spinula <n.spinula.costa@avanade.com>
2020-04-13 23:38:32 -0300

    Initial Commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/LogoCompleta.png';
+import "./styles.css";
+
+const Footer = () => (
+    <footer>
+      <p>
+        Site criado pela JuntosComVc.com, licença MIT.
+      </p>
+    </footer>
+);
+
+export default Footer;
