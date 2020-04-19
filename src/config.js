ad5dddda63b8c4c386f2d4c2f15328032d41bf31
allyhere <camilomichelettors@gmail.com>
2020-04-19 00:24:28 -0300

    atualizacao de credenciais

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ID : "296852836117-3avb3g2e846if33jg14cjb3q12h2rksu.apps.googleusercontent.com",
+  CLIENT_ID : "296852836117-g0vorrtindn1155q4ppbqldclmo5uupk.apps.googleusercontent.com",
   API_KEY : "AIzaSyBQisayvB9lKjDzhtRjR-2li6qi0xtXqzU",
   SHEET_ID :"1sFYGz3DXj691brEPkxgr1_Em5r2lxzJz5RXoV_g9mR0",
   DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
