46d13588597f60f813b3665e07ad37e8107b1db5
allyhere <camilomichelettors@gmail.com>
2020-04-18 23:03:54 -0300

    add config file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CLIENT_ID : "296852836117-3avb3g2e846if33jg14cjb3q12h2rksu.apps.googleusercontent.com",
+  API_KEY : "AIzaSyBQisayvB9lKjDzhtRjR-2li6qi0xtXqzU",
+  SHEET_ID :"1sFYGz3DXj691brEPkxgr1_Em5r2lxzJz5RXoV_g9mR0",
+  DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  SCOPE: "https://www.googleapis.com/auth/spreadsheets",
+}
